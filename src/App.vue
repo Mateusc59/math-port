19c695bddacdac42134aaa673424303ae758fa8d
Mateusc59 <mathieuhollande@hotmail.fr>
2022-09-11 15:02:22 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <NavBar />
+  <router-view />
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  height: 150vh;
+  position: relative;
+  background-color: #639d63;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="img_pea" src="../assets/logopea.png" />
+    <HelloWorld msg="Welcome to my Pearfolio" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+}
+.img_pea {
+  margin-top: 150px;
+}
+</style>
