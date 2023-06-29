875194c789f973cd45c7f183ef8e9b1ea0251e22
Mateusc59 <mathieuhollande@hotmail.fr>
2023-06-29 18:55:45 -0400

    Scrolltrigger essai 4

--- src/main.js
+++ src/main.js
@@ -1,43 +1,11 @@
 import { createApp } from "vue";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
+
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
 import "./web-components/custom-cursor/custom-cursor.js";
 import "./web-components/custom-cursor/custom-cursor";
 import "./swiper-fullscreen/swiper-fullscreen.js";
-
-document.addEventListener("DOMContentLoaded", function () {
-  gsap.registerPlugin(ScrollTrigger);
-
-  gsap.to(".hero", {
-    scrollTrigger: {
-      trigger: ".hero",
-      scrub: true,
-      pin: true,
-      start: "center center",
-      end: "bottom -100%",
-      toggleClass: { targets: ".hero", className: "active" },
-      markers: true, // Optional: Add markers to visualize the trigger
-      invalidateOnRefresh: true, // Optional: Invalidate the ScrollTrigger on refresh
-    },
-    ease: "power2",
-  });
-
-  gsap.to(".hero__headline", {
-    scrollTrigger: {
-      trigger: ".hero",
-      scrub: 0.5,
-      start: "top bottom",
-      end: "bottom -300%",
-      markers: true, // Optional
-      invalidateOnRefresh: true, // Optional
-    },
-    y: "-30%",
-    ease: "power2",
-  });
-});
 
 createApp(App).use(i18n).use(router).mount("#app");
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.to(".hero", {
+    scrollTrigger: {
+      trigger: ".hero",
+      scrub: true,
+      pin: true,
+      start: "center center",
+      end: "bottom -100%",
+      toggleClass: { targets: ".hero", className: "active" },
+      markers: true, // Optional: Add markers to visualize the trigger
+      invalidateOnRefresh: true, // Optional: Invalidate the ScrollTrigger on refresh
+    },
+    ease: "power2",
+  });
+
+  gsap.to(".hero__headline", {
+    scrollTrigger: {
+      trigger: ".hero",
+      scrub: 0.5,
+      start: "top bottom",
+      end: "bottom -300%",
+      markers: true, // Optional
+      invalidateOnRefresh: true, // Optional
+    },
+    y: "-30%",
+    ease: "power2",
+  });
+});
 
 const routes = [
   {
